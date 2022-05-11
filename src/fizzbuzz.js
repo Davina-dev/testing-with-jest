ba5223dfcc37aabd1c656071cd7dc0fca0088958
Davina-dev <davinamr91@gmail.com>
2022-05-11 18:21:53 +0200

    first commit

---
+++ src/fizzbuzz.js
@@ -0,0 +1,16 @@
+function fizzbuzz(number) {
+  if (number % 3 !== 0 && number % 5 !== 0) {
+    return number
+  }
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'Fizzbuzz'
+  }
+  if (number % 3 == 0) {
+    return 'Fizz'
+  }
+  if (number % 5 == 0) {
+    return 'Buzz'
+  }
+}
+
+module.exports = fizzbuzz
